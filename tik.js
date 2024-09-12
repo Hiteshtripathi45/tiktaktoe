18d55f3cfd62172a448be50719742212465da73d
hitesh <hiteshtripathi91@gmail.com>
2024-09-12 09:55:05 +0530

    logic for adding mark in array and inside div when clicked

--- tik.js
+++ tik.js
@@ -21,6 +21,20 @@ function players(name,mark){
 }
 
 gameplay = (function(){
+    let player1 = players('hitesh','x')
+    let player2 = players('anuj','o')
+    let tur =player1.mark
+    function turn(){
+        if(tur==player2.mark){
+            tur=player1.mark
+        } 
+        else{
+            tur=player2.mark
+        }
+    }
+    function currenturn(){
+        return tur
+    }
     function check(){
     const chances=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     let board = gameboard.getboard()
@@ -32,5 +46,18 @@ gameplay = (function(){
 
         }
     }
-    return {check}
+    return {check,turn,currenturn}
 })();
+
+const cell=Array.from (document.getElementsByClassName('cell'))
+cell.forEach(element => {
+    element.addEventListener('click',()=>{
+
+        if((gameboard.getboard()[element.id])==''){
+        element.textContent=gameplay.currenturn()
+        gameboard.update(element.id,gameplay.currenturn())
+        gameplay.turn()
+        }
+        
+    }) 
+});
